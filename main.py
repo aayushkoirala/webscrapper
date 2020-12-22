247d51e31c724553e10732aa790247e18019cf4c
Aayush <koiralaaayush1@gmail.com>
2020-12-22 11:23:22 +0000

    cleanup

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from selenium import webdriver
 import pandas as pd
-#import requests
-from bs4 import BeautifulSoup
-import os
-import csv
+#from bs4 import BeautifulSoup
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
@@ -14,28 +11,33 @@ url = 'https://mystudentrecord.ucmerced.edu/pls/PROD/xhwschedule.p_selectsubject
 
 wd = webdriver.Chrome('chromedriver', options=options)
 wd.get(url)
+
 #status = wd.find_element_by_xpath("/html/body/div[3]/form/table/tbody/tr[11]/td[1]/input").is_selected()
 #print(status)
+
 print('abc')
 wd.find_element_by_xpath("/html/body/div[3]/form/table/tbody/tr[11]/td[1]/input").click()
 
 #status = wd.find_element_by_xpath("/html/body/div[3]/form/table/tbody/tr[11]/td[1]/input").is_selected()
 #print(status)
+
 print('efg')
 wd.find_element_by_xpath("/html/body/div[3]/form/input").click()
-wd.implicitly_wait(3)
+#wd.implicitly_wait(3)
 print('1')
 html = wd.page_source
 print('2')
-dataf = pd.read_html(html)
-df = pd.concat(dataf)
+
+with open("html.txt", "w") as file:
+  file.write(html)
+
+m_df = pd.read_html(html)
+df = pd.concat(m_df)
+df.to_csv('c_df.csv')
+
 #print(df)
-df.to_csv('text.csv')
-#df.to_csv('df.csv')
-#print(df)
-#with open("df.txt", "w") as file:
- # file.write(df.to_string())
 #print(html)
+
 
 
 
