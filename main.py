9776cff45361cdfdecd1d23b24c1c92dcecbb351
Aayush <koiralaaayush1@gmail.com>
2020-12-22 09:45:54 +0000

    trying to put data frame

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ import pandas as pd
 #import requests
 from bs4 import BeautifulSoup
 
-options = webdriver.Chromeoptions()
+options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
-options.add_argument('--disable-d')
-
-url = 'https://mystudentrecord.ucmerced.edu/pls/PROD/xhwschedule.P_ViewSchedule'
-
-page = requests.get(url)
-#print(page.text)
-
-soup = BeautifulSoup(page.text, 'html.parser')
-
-print(soup)
+options.add_argument('--disable-dev-shm-usage')
 
 
+
+url = 'https://mystudentrecord.ucmerced.edu/pls/PROD/xhwschedule.p_selectsubject'
+
+wd = webdriver.Chrome('chromedriver', options=options)
+wd.get(url)
+
+
+wd.implicitly_wait(10)
+html = wd.page_source
+df = pd.read_html(html)
+
+print(df[0])
