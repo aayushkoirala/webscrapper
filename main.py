7fb478dbc955d92d29768a0160c802c1784f0c28
Aayush <koiralaaayush1@gmail.com>
2020-12-22 09:52:28 +0000

    by css selector, clicking

--- main.py
+++ main.py
@@ -18,4 +18,16 @@ term ='body > div.pagebodydiv > form > table > tbody > tr:nth-child(7) > td:nth-
 button1 = wd.find_element_by_css_selector(term)
 button1.click()
 
+all_classes ='body > div.pagebodydiv > form > table > tbody > tr:nth-child(11) > td:nth-child(1) > input[type=radio]'
 
+button2 = wd.find_element_by_css_selector(all_classes)
+button2.click()
+view = 'body > div.pagebodydiv > form > input[type=submit]'
+button3 =wd.find_element_by_css_selector(view)
+button3.click()
+
+wd.implicitly_wait(10)
+html = wd.page_source
+df = pd.read_html(html)
+
+print(df[0])
