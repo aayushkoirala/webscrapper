25df03820602a42246b3712f2a07e48f6e161ed4
Aayush <koiralaaayush1@gmail.com>
2020-12-22 10:52:28 +0000

    putting data into csv/merging

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 #import requests
 from bs4 import BeautifulSoup
 import os
+import csv
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
@@ -15,19 +16,26 @@ wd = webdriver.Chrome('chromedriver', options=options)
 wd.get(url)
 #status = wd.find_element_by_xpath("/html/body/div[3]/form/table/tbody/tr[11]/td[1]/input").is_selected()
 #print(status)
-
+print('abc')
 wd.find_element_by_xpath("/html/body/div[3]/form/table/tbody/tr[11]/td[1]/input").click()
 
 #status = wd.find_element_by_xpath("/html/body/div[3]/form/table/tbody/tr[11]/td[1]/input").is_selected()
 #print(status)
-
+print('efg')
 wd.find_element_by_xpath("/html/body/div[3]/form/input").click()
 wd.implicitly_wait(3)
+print('1')
 html = wd.page_source
+print('2')
 df = pd.read_html(html)
+
+finaldf = pd.concat(df, axis=1, join='inner').sort_index()
+finaldf.to_csv("test1.csv", index = False)
+
+#df.to_csv('df.csv')
 #print(df)
-with open("df.txt", "w") as file:
-  file.write(df)
+#with open("df.txt", "w") as file:
+ # file.write(df.to_string())
 #print(html)
 
 
