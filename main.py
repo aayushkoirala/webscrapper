5c2cf6e3e7001d8110185f1cd3bd3c2a0a4f8d9d
Aayush <koiralaaayush1@gmail.com>
2020-12-22 10:05:48 +0000

    trying to copy html to text

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from selenium import webdriver
 import pandas as pd
 #import requests
 from bs4 import BeautifulSoup
+import os
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-
-
 
 url = 'https://mystudentrecord.ucmerced.edu/pls/PROD/xhwschedule.p_selectsubject'
 
@@ -23,9 +22,9 @@ status = wd.find_element_by_xpath("/html/body/div[3]/form/table/tbody/tr[11]/td[
 print(status)
 
 wd.find_element_by_xpath("/html/body/div[3]/form/input").click()
-wd.switch_to_window(wd.window_handles[0])
+wd.implicitly_wait(3)
 html = wd.page_source
-print(html)
-
-
-
+file1=open("t.txt","w")
+file1.write(html)
+file1.close()
+#print(html)
