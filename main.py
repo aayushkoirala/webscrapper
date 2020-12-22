7e3edef1b28854ec1eb09dcd146e28bdff78df15
Aayush <koiralaaayush1@gmail.com>
2020-12-22 10:00:59 +0000

    testing other status and trying to put it in data frame

--- main.py
+++ main.py
@@ -8,12 +8,26 @@ options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
+
+
 url = 'https://mystudentrecord.ucmerced.edu/pls/PROD/xhwschedule.p_selectsubject'
 
 wd = webdriver.Chrome('chromedriver', options=options)
 wd.get(url)
 status = wd.find_element_by_xpath("/html/body/div[3]/form/table/tbody/tr[11]/td[1]/input").is_selected()
 print(status)
+
+wd.find_element_by_xpath("/html/body/div[3]/form/table/tbody/tr[11]/td[1]/input").click()
+
+status = wd.find_element_by_xpath("/html/body/div[3]/form/table/tbody/tr[11]/td[1]/input").is_selected()
+print(status)
+
+wd.find_element_by_xpath("/html/body/div[3]/form/input").click()
+wd.implicitly_wait(10)
+html = wd.page_source
+df = wd.read_html(html)
+
+print(df[0])
 
 
 
