ab1b4ab63c872f55b63e76391534b2c6e8ae0677
Aayush <koiralaaayush1@gmail.com>
2020-12-22 09:57:09 +0000

    changing the path, using xpath to find button order and checking it's status.

--- main.py
+++ main.py
@@ -8,26 +8,12 @@ options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
-
-
-url = 'https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_base_stats_(Generation_VI)'
+url = 'https://mystudentrecord.ucmerced.edu/pls/PROD/xhwschedule.p_selectsubject'
 
 wd = webdriver.Chrome('chromedriver', options=options)
 wd.get(url)
-term ='body > div.pagebodydiv > form > table > tbody > tr:nth-child(7) > td:nth-child(1) > input[type=radio]'
-button1 = wd.find_element_by_css_selector(term)
-button1.click()
+status = wd.find_element_by_xpath("/html/body/div[3]/form/table/tbody/tr[11]/td[1]/input").is_selected()
+print(status)
 
-all_classes ='body > div.pagebodydiv > form > table > tbody > tr:nth-child(11) > td:nth-child(1) > input[type=radio]'
 
-button2 = wd.find_element_by_css_selector(all_classes)
-button2.click()
-view = 'body > div.pagebodydiv > form > input[type=submit]'
-button3 =wd.find_element_by_css_selector(view)
-button3.click()
 
-wd.implicitly_wait(10)
-html = wd.page_source
-df = pd.read_html(html)
-
-print(df[0])
